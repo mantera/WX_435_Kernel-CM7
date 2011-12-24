cc75cab1ba7ee4fcf351d8f7b63ccd3b63360e00
mantera <som4th@gmail.com>
2011-12-23 22:48:08 -0500

    revert memcopy tweak.  Was causing instability at higher clocks.

--- lib/string.c
+++ lib/string.c
@@ -23,7 +23,6 @@
 #include <linux/string.h>
 #include <linux/ctype.h>
 #include <linux/module.h>
-#include <linux/memcopy.h>
 
 #ifndef __HAVE_ARCH_STRNICMP
 /**
@@ -562,12 +561,10 @@ EXPORT_SYMBOL(memset);
 void *memcpy(void *dest, const void *src, size_t count)
 {
 	char *tmp = dest;
-	unsigned long dstp = (unsigned long)dest;
-	unsigned long srcp = (unsigned long)src;
+	const char *s = src;
 
 	while (count--)
-	/* Copy from the beginning to the end */
-	mem_copy_fwd(dstp, srcp, count);
+	  *tmp++ = *s++;
 	return dest;
 }
 EXPORT_SYMBOL(memcpy);
@@ -585,15 +582,20 @@ EXPORT_SYMBOL(memcpy);
 void *memmove(void *dest, const void *src, size_t count)
 {
 	char *tmp;
-	unsigned long dstp = (unsigned long)dest; 
-	unsigned long srcp = (unsigned long)src; 
+	const char *s;
 
-	if (dest - src >= count) { 
-	/* Copy from the beginning to the end */ 
-		mem_copy_fwd(dstp, srcp, count); 
+	if (dest <= src) { 
+	  tmp = dest; 
+	  s = src; 
+	  while (count--) 
+	    *tmp++ = *s++; 
 	} else {
-		/* Copy from the end to the beginning */ 
-		mem_copy_bwd(dstp, srcp, count); 
+	  tmp = dest; 
+	  tmp += count; 
+	  s = src; 
+	  s += count; 
+	  while (count--) 
+	  *--tmp = *--s; 
 	}
 	return dest;
 }
